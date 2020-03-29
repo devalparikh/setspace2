4ba389b1167e8a5d3c9fb965c6fe25e0a70d4421
Deval Parikh <devalpp@gmail.com>
2020-03-29 15:32:51 -0400

    landing page init

---
+++ src/MyTheme.js
@@ -0,0 +1,37 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import shadows from '@material-ui/core/styles/shadows';
+
+export default createMuiTheme({
+    palette: {
+        primary: { // works
+          main: '#165788',
+          contrastText: '#fff',
+        },
+        secondary: { // works
+          main: '#69BE28',
+          contrastText: '#fff',
+        },
+        companyBlue: { // doesnt work - defaults to a grey button
+            main: '#65CFE9',
+            contrastText: '#fff',
+        },
+        companyRed: { // doesnt work - grey button
+            main: '#E44D69',
+            contrastText: '#000',
+        },
+        appBar: { 
+            backgroundColor: '#282c3400',
+            fontWeight: '700',
+            boxShadow: 'none',
+            color: '#fff'
+        },
+        wideBtn: {
+            backgroundColor: '#282c34',
+            color: 'white',
+            width: '19.5rem',
+            height: '50px',
+            border: '3px',
+        }
+    },
+});
