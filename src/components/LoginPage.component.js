02558e15b45ac9bc43022208efe5f530963dbcf2
Steve Sahayadarlin <jsahayad@masonlive.gmu.edu>
2020-03-30 16:20:30 -0400

    added description for getting to sign up page from login page

--- src/components/LoginPage.component.js
+++ src/components/LoginPage.component.js
@@ -171,7 +171,7 @@ const LandingPage = () => {
                     </p>
                     <GoogleLoginButton align="center"  text="Continue with Google" className="social-sign-in-btn" onClick={() => alert("Hello Google Log in")} />
                     <FacebookLoginButton align="center" text="Continue with Facebook" className="social-sign-in-btn" onClick={() => alert("Hello Facebook Log in")} />
-                    <center><a style={{ margin: "5rem" }} href="/">Sign Up</a></center>
+                    <center> Don't have an account? <a href="/">Sign Up</a></center>
 
                 </Card>
             </section>
